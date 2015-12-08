fbb1d56d41ccbf8419c2af812ee6cc7ef83e684e
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2015-12-08 22:32:13 +0000

    Simplify template using "for x in y"

--- client/application.js
+++ client/application.js
@@ -6,8 +6,8 @@ if (Meteor.isClient) {
       'sourceUrl': 'http://time.com/3018265/george-harrison-memorial-tree-beetles/',
       'source': 'Time.com',
       'paragraphs': [
-        {'text': 'In a terrible case of irony, a tree planted in memory of Beatles guitarist George Harrison in Los Angeles has been destroyed by literal beetles.'},
-        {'text': 'The pine tree was planted in Griffith Park in 2004 in honor of the musician and avid gardener, who resided in LA at the end of his life.'}
+        'In a terrible case of irony, a tree planted in memory of Beatles guitarist George Harrison in Los Angeles has been destroyed by literal beetles.',
+        'The pine tree was planted in Griffith Park in 2004 in honor of the musician and avid gardener, who resided in LA at the end of his life.'
       ]
     }]
   });
