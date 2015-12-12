0121930c8444eb6528be5519100b1b2b5c7366c2
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2015-12-12 22:05:54 +0000

    Second attempt at CI
    Meteor installation failed on previous attempt, so trying with sudo privileges

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Headline of the Year
 
+[![Build Status](https://travis-ci.org/textbook/headlineOfTheYear.svg?branch=master)][travis]
+
 A [Meteor]-based web application for tracking the finest examples of the 
 sub-editor's art. Eventually, I'm aiming to allow users to compare pairs of 
 headlines, figuring out which are the most popular for a given year.
@@ -10,3 +12,4 @@ View the task list and progress on [Pivotal Tracker][tracker].
 
   [meteor]: https://www.meteor.com/
   [tracker]: https://www.pivotaltracker.com/projects/1498768
+  [travis]: https://travis-ci.org/textbook/headlineOfTheYear
--- client/client.js
+++ client/client.js
@@ -1,3 +1,6 @@
+var LEFT_ARROW = 37,
+    RIGHT_ARROW = 39;
+
 if (Meteor.isClient) {
   HistoricalArticles = new Mongo.Collection('historical-articles');
 
@@ -9,11 +12,29 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.historical.events({
+    'click .headline': function () {
+      console.log(this._id);
+    }
+  });
+
   Template.registerHelper('prettifyDate', function (format, timestamp) {
     return (timestamp ? moment(timestamp) : moment()).format(format ? format : 'YYYY/MM/DD');
   });
 
   Template.registerHelper('uppercase', function (text) {
     return text.toUpperCase();
+  });
+
+  Meteor.startup(function () {
+
+    // Handle keypress events in the whole page
+    $('body').on('keyup', function (event) {
+      if (event.keyCode === LEFT_ARROW) {
+        console.log('left arrow');
+      } else if (event.keyCode === RIGHT_ARROW) {
+        console.log('right arrow');
+      }
+    });
   });
 }
